51233bece9cbd90b312a576dbd1c3e17b25476b0
Jean Rafael Guilouski Wojcik <68492594+JeanRGW@users.noreply.github.com>
2024-10-22 20:45:38 -0300

    Update index.js

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Sobre = () =>
     <div>
-        <h1>Bem-vindo à página CONTATO</h1>
+        <h1>Bem-vindo à página SOBRE</h1>
 
         <Link to='/'>Home</Link>
         <Link to='/contato'>Contato</Link>
